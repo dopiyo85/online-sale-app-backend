b97a2a28bc459a61644ffce45896731b4ff85afd
Diana Opiyo <dopiyo@gmail.com>
2023-03-15 12:15:09 +0300

    created backend app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+
+    mongoDB_URI: 'mongodb+srv://dopiyo:Glorified30@online-sale-app.cy3of0t.mongodb.net/test'
+ 
+ }
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// mongoose.set('StrictQuery', true);
+
+const productRoute = require('./routes/productRoute');
+
+// Initializing express
+const app = express()
+
+// Body parser middleware
+app.use(express.json())
+
+//DB config
+
+const MONGODB_URI= process.env.MONGODB_URI ||
+
+require('./config').mongoDB_URI
+
+// Connect to MongoDB
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true  })
+
+// Check Connection
+
+let db = mongoose.connection;
+
+db.once('open', ()=>{
+
+   console.log('Database connected successfully')
+
+})
+
+// Check for DB Errors
+
+db.on('error', (error)=>{
+
+   console.log(error);
+
+   // Use Routes
+
+ })
+ app.use('/product', productRoute)  //new code
+
+//  // Serve our static assets if in production
+// if(process.env.NODE_ENV === 'production'){
+//    // set a static folder
+//    app.use(express.static('client/build'));
+
+//    app.get('*', (req,res)=>{
+//        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//    })
+
+// }
+ 
+// Define the PORT
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+   console.log(`Server listening on port ${PORT}`)
+})
+
